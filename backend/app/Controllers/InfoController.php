65df88fba5a2d3012a3c237727d9dbda9995423b
Myko Piolo Feudo <144913249+feudomykopiolo@users.noreply.github.com>
2023-12-23 22:18:56 +0800

    Update InfoController.php

---
+++ backend/app/Controllers/InfoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\HTTP\Response;
+use CodeIgniter\RestFul\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\InfoModel;
+use App\Controllers\BaseController;
+
+class InfoController extends ResourceController
+{
+    public function index()
+    {
+        //
+    }
+    public function getInfos(){
+        $job = new InfoModel();
+        $data = $job->findAll();
+        return $this->respond($data, 200);
+    }
+    
+}
